6434ea267c7657aa5b546630d8cc94a8995912fe
Keneisha Wiggan <Keneishawiggan8@gmail.com>
2019-11-06 09:01:12 -0500

    Initial Commit

---
+++ C-Organizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C-Organizer
+//
+//  Created by Keneisha Wiggan on 11/6/19.
+//  Copyright © 2019 Keneisha Wiggan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
