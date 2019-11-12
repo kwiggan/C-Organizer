a464a8dc247d1621a2dc1d5e98e962de35ac7233
Shantal Ewell <shantal.ewell@yahoo.com>
2019-11-12 00:57:33 -0500

    I have added the basic user interface and also the selection of the classes

---
+++ C-Organizer/ShantalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShantalViewController.swift
+//  C-Organizer
+//
+//  Created by Emmanuel Gyekye Atta-Penkra on 11/11/19.
+//  Copyright © 2019 Keneisha Wiggan. All rights reserved.
+//
+
+import UIKit
+
+class ShantalViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
