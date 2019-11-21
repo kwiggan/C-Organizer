612be913da41f85209b2d114f2feabde55a98a61
Shantal <shantal.ewell@yahoo.com>
2019-11-20 22:56:37 -0500

    New Updates

--- AllNamesViewController.swift
+++ AllNamesViewController.swift
@@ -10,32 +10,62 @@ import UIKit
 
 class AllNamesViewController: UIViewController {
 
-    var sNames = ["Shantal Ewell", "Keneisha Wiggan", "Amarachi Kalu-Onuma", "Ikenna Brown", "Ruby Birchiet"]
+//    var sNames = ["Shantal Ewell", "Keneisha Wiggan", "Amarachi Kalu-Onuma", "Ikenna Brown", "Ruby Birchiet"]
+    var students = [
+        Student("Shantal Ewell"),
+        Student("Keneisha Wiggan"),
+        Student("Amarachi Kalu-Onuma"),
+        Student("Ikenna Brown"),
+        Student("Ruby Birchiet")
+    ]
+    
+   // let test = Array(sNames.filter { !" \n\t\r".contains($0) })
+    
+   
+    @IBOutlet weak var averageLabel: UILabel!
     
     var sum = 0
     var segue = ""
     
+//    let myString = " Hello World ! "
+//    let formattedString = myString.replacingOccurrences(of: " ", with: "")
+ 
+    @IBOutlet weak var studentsTable: UITableView!
+    @IBOutlet weak var topicLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        averageLabel.isHidden = true
         
         switch segue{
         case "long":
-            if let max = sNames.max(by: {$1.count > $0.count}) {
-                print(max)
+             topicLabel.text = "Longest Name"
+            if let max = students.max(by: {$1.name.count > $0.name.count}) {
+                for (index, student) in students.enumerated() {
+                    if student.length == max.length {
+                        student.isLongestName = true
+                    }
+                }
             }
+            
+            
             case "total":
-            for character in sNames{
-                let charCount = character.count
-                sum = charCount
-                print("The total character is: \(sum)")
-            }
+                 topicLabel.text = "Total Character in Names"
+                for student in students {
+                    let charCount = student.name.count
+                    sum = charCount
+                    print("The total character in \(student.name) is \(sum)")
+                }
+            
         case "average":
-            for character in sNames{
-            let charCount = character.count
+             topicLabel.text = "Average of All Names"
+            averageLabel.isHidden = false
+            for student in students {
+                let charCount = student.name.count
             sum += charCount
             }
-            let average:Double = Double(sum/sNames.count)
-            print("The average of the names is: \(average)")
+            let average:Double = Double(sum/students.count)
+            averageLabel.text = "Average: \(average)"
             
         default:
             break
@@ -47,17 +77,43 @@ class AllNamesViewController: UIViewController {
 
 extension AllNamesViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sNames.count
+        return students.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! Students
-        cell.name.text = sNames[indexPath.row]
+        cell.name.text = students[indexPath.row].name
+        
+        if students[indexPath.row].isLongestName {
+            cell.background.backgroundColor = UIColor.black
+        }
+        
+        if segue == "total" {
+            cell.totalChars.text = "Total char: \(students[indexPath.row].length)"
+        }else {
+            cell.totalChars.isHidden = true
+        }
+        
         return cell
     }
 }
 
 class Students: UITableViewCell {
     
+    @IBOutlet weak var background: UIView!
     @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var totalChars: UILabel!
 }
+
+class Student {
+    var name: String
+    var length: Int
+    var isLongestName = false
+    
+    init(_ name: String){
+        self.name = name
+        self.length = name.count
+    }
+}
+
+
