048c9b4fbe810e98c4b5506273f4afd196c171f7
Keneisha Wiggan <keneishawiggan8@gmail.com>
2019-12-08 11:45:20 -0500

    Created a quiz feature for the application where teacher can give class a system generated quiz. The teacher can type questions that the teacher would like to appear on the next quiz.

--- C-Organizer/ViewController.swift
+++ C-Organizer/ViewController.swift
@@ -9,13 +9,85 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
+    let questions = ["What is Scrum?", "Whats in sprint review?", "where is claflin located?"]
+    let answers = [["Agile Methodology", "An App", "Sprint Review"], ["userstory","name","grade"], ["Orangeburg","New York","Tennesse"]]
+    
+    //Varibles
+    var currentQuestion = 0
+    var rightAnswerPlacement:UInt32 = 0
+    //variable the counts the number of points
+    var points = 0;
+    var score: Int = 0
+    
+    
+//Label
+    @IBOutlet weak var label: UILabel!
+   
+    //Button
+    @IBAction func action(_ sender: Any)
+    {
+        if ((sender as AnyObject).tag == Int(rightAnswerPlacement))
+        {
+            print("Right answer! Good Job!")
+            points += 1
+        }
+        else
+        {
+            print ("Wrong Answer!, try again")
+        }
+        if (currentQuestion != questions.count)
+        {
+            newQuestions()
+        }
+            
+        else
+           {
+           performSegue(withIdentifier: "showScore", sender: self)
+           }
+    }
+    override func viewDidAppear(_ animated: Bool)
+    {
+        newQuestions()
+    }
+   
+    //Function that displays questions
+   func newQuestions()
+   {
+    label.text = questions[currentQuestion]
+    
+    rightAnswerPlacement = arc4random_uniform(3)+1
+    
+    //Create a button
+    var button:UIButton = UIButton()
+    
+    var x = 1
+    for i in 1...3
+    {
+        //Create a button
+        button = view.viewWithTag(i) as! UIButton
+        if (i == Int(rightAnswerPlacement))
+        {
+            button.setTitle(answers[currentQuestion][0], for: .normal)
+        }
+        else
+        {
+            button.setTitle(answers[currentQuestion][x], for: .normal)
+            x = 2
+        }
+       
+    }
+     currentQuestion += 1
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //testing
-        print("sam")
+       
     }
 
 
